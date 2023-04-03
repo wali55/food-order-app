3ba90a2e7246847ea766d6a29a0448db50ac9af1
tonic-6101 <tonicxx341@gmail.com>
2023-04-03 10:14:20 +0200

    added function showCartHandler show and hide

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import Cart from './components/Cart/Cart';
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
       <Cart />
