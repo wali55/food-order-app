1c6f8935f1553f1579cfa4e69b9522e67e604304
tonic-6101 <tonicxx341@gmail.com>
2023-04-04 07:34:44 +0200

    added props.onClose

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <Fragment>
-      {cartIsShown && <Cart />}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
