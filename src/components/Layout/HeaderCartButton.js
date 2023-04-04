029ed0459ac1715ccecb00cfd872c6b66a564058
tonic-6101 <tonicxx341@gmail.com>
2023-04-04 08:03:32 +0200

    establishing conection beetween provider and headerCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
