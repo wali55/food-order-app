52b802b1a076b4a0b90452a501a17979ad461102
tonic-6101 <tonicxx341@gmail.com>
2023-04-21 12:35:12 +0200

    added form and cancelation

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-http-36787-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wring!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -30,13 +36,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
-      <section className="classes.MealsLoading">
+      <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
